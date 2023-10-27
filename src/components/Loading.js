d44ff9fcfd59239e4e5a8167919e5db1ed770a01
pradip.uniqual <pradip.uniqual@gmail.com>
2023-10-27 18:23:35 +0530

    fix: customize

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import pradipLadva from "../resources/images/PradipLogo.svg"
-import '../styles/Loading.css'
+import React from "react";
+import pradipLadva from "../resources/images/PradipLogo.svg";
+import "../styles/Loading.css";
 
 const Loading = () => {
   return (
     <div className="preloader">
       <img src={pradipLadva} alt="pradipLadva" />
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
